25b62ae4928d93f86402affce31e10c816876648
Eliseo Martelli <me@eliseomartelli.it>
2020-08-13 11:20:06 +0200

    feat(tests): add tests

---
+++ tests/test_stop.py
@@ -0,0 +1,23 @@
+import pytest
+from pygtt import *
+from datetime import datetime
+
+def test_stop():
+    stop = Stop("1234", [])
+    assert stop.name == "1234"
+    assert stop.bus_list == []
+    assert stop.next == None
+
+def test_stop_next():
+    stop = Stop("1234", [
+        Bus("1", [
+            datetime.fromtimestamp(1237),
+            datetime.fromtimestamp(1236)
+        ]),
+        Bus("2", [
+            datetime.fromtimestamp(1235),
+            datetime.fromtimestamp(1234)
+        ]),
+    ])
+    assert stop.next.name == "2"
+    assert stop.next.first_time == datetime.fromtimestamp(1234)
